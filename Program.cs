87b036723ec24e39bd8d22f08b0f9e83d2dde3c5
SAMUEL ALEJANDRO HURTADO JIMENEZ <samuel.hurtado@nexti.com>
2023-01-16 09:57:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string name = "First example";
+string result = string.Empty;
+
+// one
+void String_Reverse_FOR()
+{
+    for (int i = 0; i < name.Length; i++)
+    {
+        result = $"{name[i]}{result}";
+    }
+    Console.WriteLine(result);
+}
+
+// two
+void CharArray_Reverse()
+{
+    char[] cName = name.ToCharArray();
+    Array.Reverse(cName);
+    Console.WriteLine(cName);
+    result = new String(cName);
+    Console.WriteLine(result);
+
+}
