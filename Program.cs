6372df27b1802a142b0e7764c04fe6f7ccedf2d2
SAMUEL ALEJANDRO HURTADO JIMENEZ <samuel.hurtado@nexti.com>
2023-01-17 11:32:13 -0500

    hamming distance

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BasicExamples;
 using System.Text.RegularExpressions;
 
 Console.WriteLine("Hello, World!");
@@ -53,3 +54,18 @@ void CountCharacterRepeated_Linq()
 
 }
 
+HammingDistance();
+void HammingDistance()
+{
+    string wordOne = "Canibales";
+    string wordTwo = "Hanibalos";
+    int distance = 0;
+    if (wordOne.Length != wordTwo.Length) throw new Exception("the words does not have the same length");
+
+    for (int i = 0; i < wordOne.Length; i++)
+    {
+        if (wordOne[i] != wordTwo[i])
+            distance++;
+    }
+    Console.WriteLine($"Hamming Distance is: {distance}, between: {wordOne} and {wordTwo}.");
+}
